231cfb4c8507a3105c3dfccb8c21fe27c1661a8a
Lexicon Student 2 <Lexicon Student 2@DESKTOP-2GQ78GN>
2022-09-02 11:10:49 +0200

    Add project files.

---
+++ Lms.Core/Enteties/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Enteties
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public DateTime StartDate { get; set; }
+        public ICollection<Module> Modules{ get; set; } = new List<Module>();
+    }
+}
---
+++ Lms.Core/Enteties/Module.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Enteties
+{
+    public class Module
+    {
+        public int Id;
+        public string Title { get; set; } = String.Empty;   
+        public DateTime StartDate { get; set; }
+        public int CourseId;
+    }
+}
